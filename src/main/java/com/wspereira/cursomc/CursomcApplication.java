b8b4f0eb5a77546672fcdb84d2e8dcd423deb5a3
Wanderson Pereira <ws-pereira@hotmail.com>
2022-01-27 14:48:57 -0300

    Associação muitos para muitos entre Produto x Categoria

--- src/main/java/com/wspereira/cursomc/CursomcApplication.java
+++ src/main/java/com/wspereira/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.wspereira.cursomc.domain.Categoria;
+import com.wspereira.cursomc.domain.Produto;
 import com.wspereira.cursomc.repositories.CategoriaRepository;
+import com.wspereira.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository; 
+	@Autowired
+	private ProdutoRepository produtoRepository; 
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,8 +32,28 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat2 = new Categoria(null, "Escritório");
 		Categoria cat3 = new Categoria(null, "Papelaria");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		Produto p1 = new Produto(null, "Notebook", 4500.00);
+		Produto p2 = new Produto(null, "Cadeira", 150.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		Produto p4 = new Produto(null, "Impressora", 800.00);
+		Produto p5 = new Produto(null, "Resma A4", 15.99);
 		
+		// Iniciando a criação de associações entre Categorais x Produtos
+		cat1.getProdutos().addAll(Arrays.asList(p1,p3,p4));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat3.getProdutos().addAll(Arrays.asList(p4));
+		
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p4.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p5.getCategorias().addAll(Arrays.asList(cat3));
+		
+		
+		// Salvando os dados
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 	}
 	
 	
--- src/main/java/com/wspereira/cursomc/domain/Categoria.java
+++ src/main/java/com/wspereira/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.wspereira.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 @Entity
@@ -17,12 +20,20 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+
+	
+	
+	
+	@ManyToMany(mappedBy = "categorias")                   //Fazendo o mapeamento inveso com a classe e tabela categorias
+	private List<Produto> produtos = new ArrayList<>();   // Fazendo associação com a classe categoria e inicinado as coleções
+	
 	
 	public Categoria() {
 		
 	}
 
 	public Categoria(Integer id, String name) {
+		super();
 		this.id = id;
 		this.name = name;
 	}
@@ -46,6 +57,16 @@ public class Categoria implements Serializable{
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+
+	
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
